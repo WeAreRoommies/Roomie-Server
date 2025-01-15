e030afad358c3b7b8c90bc6854c70b97949215a0
Yeonnies <eliyeon16@gmail.com>
2025-01-15 18:11:51 +0900

    fix repository

--- producer/src/main/java/server/producer/domain/controller/HouseController.java
+++ producer/src/main/java/server/producer/domain/controller/HouseController.java
@@ -1,36 +1,24 @@
 package server.producer.domain.controller;
 
-<<<<<<< HEAD
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-=======
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
->>>>>>> 163576d776bf6e995e70762e4b20f1a484fc80fb
 import org.springframework.web.bind.annotation.RestController;
 import server.producer.common.dto.ApiResponseDto;
 import server.producer.common.dto.enums.ErrorCode;
 import server.producer.common.dto.enums.SuccessCode;
-<<<<<<< HEAD
-import server.producer.domain.dto.response.HomeInfoResponseDto;
 import server.producer.domain.dto.response.HouseDetailsResponseDto;
-=======
 import server.producer.domain.dto.response.MoodHouseResponseDto;
->>>>>>> 163576d776bf6e995e70762e4b20f1a484fc80fb
 import server.producer.domain.service.HouseService;
 
 @RestController
 @RequestMapping("/v1/houses")
-<<<<<<< HEAD
+@RequiredArgsConstructor
 public class HouseController {
     private final HouseService houseService;
-    public HouseController(HouseService houseService) {
-        this.houseService = houseService;
-    }
+
     private final Long userId = 1L;
 
     @GetMapping("/{houseId}/details")
@@ -42,13 +30,6 @@ public class HouseController {
             return ApiResponseDto.fail(ErrorCode.INTERNAL_SERVER_ERROR);
         }
     }
-
-=======
-@RequiredArgsConstructor
-public class HouseController {
-	private final HouseService houseService;
-	private final Long userId = 1L;
-
 	@GetMapping
 	public ApiResponseDto<MoodHouseResponseDto> getHousesByMoodAndLocation(@RequestParam String moodTag) {
 		try {
@@ -58,5 +39,4 @@ public class HouseController {
 			return ApiResponseDto.fail(ErrorCode.NOT_FOUND_HOUSE);
 		}
 	}
->>>>>>> 163576d776bf6e995e70762e4b20f1a484fc80fb
 }
