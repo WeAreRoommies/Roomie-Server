5549533f942e18fe0e35f78cceed669517b6bd8f
mr8356 <mr8356@naver.com>
2025-01-19 20:50:30 +0900

    fix contoller mapping

--- producer/src/main/java/server/producer/external/controller/naver/NaverLocationController.java
+++ producer/src/main/java/server/producer/external/controller/naver/NaverLocationController.java
@@ -1,9 +1,9 @@
 package server.producer.external.controller.naver;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import server.producer.external.controller.naver.dto.LocationsDto;
@@ -11,16 +11,19 @@ import server.producer.external.service.naver.NaverService;
 
 
 @RestController
-@RequestMapping("/v1/locations")
 @RequiredArgsConstructor
+@Slf4j
 public class NaverLocationController {
 
     private final NaverService naverService;
 
-    @GetMapping
+    @GetMapping("/v1/locations")
     public ResponseEntity<LocationsDto> getLocations(
             @RequestParam(name = "q") final String query
     ) {
-        return ResponseEntity.ok(naverService.getLocations(query));
+        log.info("Get locations from Naver");
+        log.info("query: {}", query);
+        log.info("naverService: {}", naverService);
+        return ResponseEntity.ok(naverService.getLocations(query)); d
     }
 }
