b8bb68e0bf0a9e33a77489557c7ada2a88bc57a4
mr8356 <mr8356@naver.com>
2025-01-21 00:45:00 +0900

    fix pin repository

--- consumer/src/main/java/server/consumer/scheduler/TourQueueScheduler.java
+++ consumer/src/main/java/server/consumer/scheduler/TourQueueScheduler.java
@@ -1,12 +1,14 @@
 package server.consumer.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import server.consumer.processor.TourQueueProcessor;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class TourQueueScheduler {
 
 	private final TourQueueProcessor tourQueueProcessor;
@@ -14,6 +16,7 @@ public class TourQueueScheduler {
 	@Scheduled(fixedDelay = 3000)
 	public void consumeMessages() {
 		String queueName = "tourRequest";
+		log.info("Consuming messages from queue: {}", queueName);
 		tourQueueProcessor.processMessage(queueName);
 	}
 }
