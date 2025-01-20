b546a4b0518a378a84c9e450c55c7a29198779c3
mr8356 <mr8356@naver.com>
2025-01-20 09:05:19 +0900

    modify naver

--- producer/src/main/java/server/producer/external/controller/naver/NaverLocationController.java
+++ producer/src/main/java/server/producer/external/controller/naver/NaverLocationController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import server.producer.common.dto.ApiResponseDto;
+import server.producer.common.dto.enums.SuccessCode;
 import server.producer.external.controller.naver.dto.LocationsDto;
 import server.producer.external.service.naver.NaverService;
 
@@ -18,13 +20,13 @@ public class NaverLocationController {
     private final NaverService naverService;
 
     @GetMapping("/v1/locations")
-    public ResponseEntity<LocationsDto> getLocations(
+    public ApiResponseDto<LocationsDto> getLocations(
             @RequestParam(name = "q") final String query
     ) {
         log.info("Get locations from Naver");
         log.info("query: {}", query);
         log.info("naverService: {}", naverService);
         System.out.println("logtest");
-        return ResponseEntity.ok(naverService.getLocations(query));
+        return ApiResponseDto.success(SuccessCode.HOUSE_GET_SUCCESS, naverService.getLocations(query));
     }
 }
