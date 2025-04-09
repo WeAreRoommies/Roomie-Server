6c3f706f0b969a67cf8ffbba2f6ddff9ad1d107b
mr8356 <mr8356@naver.com>
2025-04-10 00:07:16 +0900

    fix user entity Builder fail

--- common/src/main/java/entity/User.java
+++ common/src/main/java/entity/User.java
@@ -6,32 +6,36 @@ import lombok.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name="user")
-@Data
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String email;
-
     private String socialId;
 
     @Enumerated(EnumType.STRING)
     private SocialType socialType;
+
     @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
     private String location;
 
+    @Builder.Default
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Pin> pins = new ArrayList<>();
 
+    @Builder.Default
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<RecentlyViewedHouse> recentlyViewedHouses = new ArrayList<>();
 }
