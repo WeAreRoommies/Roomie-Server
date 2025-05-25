fe48098d2ade98c68fbcbf250bc24f5cf4caeff3
mr8356 <mr8356@naver.com>
2025-05-25 15:16:29 +0900

    fix setter from jwt

--- producer/src/main/java/server/producer/security/jwt/JwtProperties.java
+++ producer/src/main/java/server/producer/security/jwt/JwtProperties.java
@@ -1,19 +1,22 @@
 package server.producer.security.jwt;
+
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "jwt")
-@Getter
 public class JwtProperties {
-    private String secretKey;
+    private String secret;
     private Expiration expiration;
 
     @Getter
+    @Setter
     public static class Expiration {
-        private long access;
-        private long refresh;
+        private long access;  // access token 만료 시간
     }
 }
 
