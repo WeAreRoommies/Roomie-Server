2593e4ad989c17a67e28d304d07044ce0a66aad5
Yeonnies <eliyeon16@gmail.com>
2025-05-28 02:46:30 +0900

    #90 로그인 삭제

--- producer/src/main/java/server/producer/domain/service/SocialLoginService.java
+++ producer/src/main/java/server/producer/domain/service/SocialLoginService.java
@@ -21,50 +21,6 @@ public class SocialLoginService {
 	private final JwtTokenProvider jwtProvider;
 	private final RefreshTokenRepository refreshTokenRepository;
 
-	public SocialLoginResponseDto login(String provider, String accessToken) {
-		SocialLoginStrategy strategy = getStrategy(provider);
-		SocialUserInfo userInfo = strategy.getUserInfo(accessToken);
-
-		User user = userRepository.findBySocialTypeAndSocialId(userInfo.getProvider(), userInfo.getId())
-				.orElseThrow(() -> new RuntimeException("회원가입 필요"));
-
-		String access = jwtProvider.createToken(user);
-		String refresh = jwtProvider.createRefreshToken(user);
-		refreshTokenRepository.save(refresh, user.getId());
-
-		return SocialLoginResponseDto.builder()
-				.accessToken(access)
-				.refreshToken(refresh)
-				.build();
-	}
-
-	public SocialSignupResponseDto signup(String provider, String accessToken, String nickname) {
-		SocialLoginStrategy strategy = getStrategy(provider);
-		SocialUserInfo userInfo = strategy.getUserInfo(accessToken);
-
-		if (userRepository.findBySocialTypeAndSocialId(userInfo.getProvider(), userInfo.getId()).isPresent()) {
-			throw new RuntimeException("이미 가입된 유저입니다");
-		}
-
-		User newUser = User.builder()
-				.email(userInfo.getEmail())
-				.nickname(nickname)
-				.socialType(userInfo.getProvider())
-				.socialId(userInfo.getId())
-				.build();
-
-		userRepository.save(newUser);
-
-		String access = jwtProvider.createToken(newUser);
-		String refresh = jwtProvider.createRefreshToken(newUser);
-		refreshTokenRepository.save(refresh, newUser.getId());
-
-		return SocialSignupResponseDto.builder()
-				.accessToken(access)
-				.refreshToken(refresh)
-				.build();
-	}
-
 	public SocialLoginResponseDto loginOrSignup(String provider, String accessToken) {
 		SocialLoginStrategy strategy = getStrategy(provider);
 		SocialUserInfo userInfo = strategy.getUserInfo(accessToken);
