1064a0df86b7acc206e2192d26cdb26551e19af4
Yeonnies <eliyeon16@gmail.com>
2025-06-28 01:38:33 +0900

    #113 유저 정보 수정 오류 해결

--- producer/src/main/java/server/producer/domain/dto/response/NameUpdateResponseDto.java
+++ producer/src/main/java/server/producer/domain/dto/response/NameUpdateResponseDto.java
@@ -1,5 +1,11 @@
 package server.producer.domain.dto.response;
 
+import lombok.Builder;
+import org.springframework.transaction.annotation.Transactional;
+
 public record NameUpdateResponseDto(
         String name
-) {}
+) {
+    @Builder
+    public NameUpdateResponseDto {}
+}
--- producer/src/main/java/server/producer/domain/dto/response/NicknameUpdateResponseDto.java
+++ producer/src/main/java/server/producer/domain/dto/response/NicknameUpdateResponseDto.java
@@ -1,5 +1,10 @@
 package server.producer.domain.dto.response;
 
+import lombok.Builder;
+
 public record NicknameUpdateResponseDto(
         String nickname
-) {}
+) {
+    @Builder
+    public NicknameUpdateResponseDto {}
+}
